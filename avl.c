29ab0eb3b8419ad41fe6a79e8902183ab0f6d2fa
Carlos Eduardo Bahia <cadu29bahia@gmail.com>
2024-02-06 19:39:32 -0300

    AVL - Efficient edition

--- avl.c
+++ avl.c
@@ -2,21 +2,49 @@
 #include <stdlib.h>
 #include "avl.h"
 
-tree_avl insert_avl(tree_avl root, int value){
+tree_avl insert_avl(tree_avl root, int value, int *insert_avl_factor){
     if(root == NULL){
         tree_avl new = (tree_avl) malloc(sizeof(node_avl));
         new->value = value;
+        new->factor = 0;
         new->left = NULL;
+        new->right = NULL;
+
+        *insert_avl_factor = 1;
         return new;
     }
 
     if(value < root->value){
-        root->left = insert_avl(root->left, value);
-    } else if(value >= root->value){
-        root->right = insert_avl(root->right, value);
+        root->left = insert_avl(root->left, value, insert_avl_factor);
+        if(*insert_avl_factor == 1){
+            if(root->factor == 0){
+                root->factor = -1;
+                *insert_avl_factor = 1;
+            } else if (root->factor == 1){
+                root->factor = 0;
+                *insert_avl_factor = 0;
+            } else {
+                *insert_avl_factor = 0;
+                return balance_avl(root);
+            }
+        }
+    } else{
+        root->right = insert_avl(root->right, value, insert_avl_factor);
+        if(*insert_avl_factor == 1){
+            if(root->factor == 0){
+                root->factor = 1;
+                *insert_avl_factor = 1;
+            } else if (root->factor == -1){
+                root->factor = 0;
+                *insert_avl_factor = 0;
+            } else {
+                *insert_avl_factor = 0;
+                return balance_avl(root);
+            }
+        }
     }
 
-    return balance_avl(root);
+    return root;
 }
 
 tree_avl balance_avl(tree_avl root){
@@ -24,16 +52,18 @@ tree_avl balance_avl(tree_avl root){
         return NULL;
     }
 
-    if(getFactor(root) == -2){
-        if(getFactor(root->left) == 1){
-            root->left = rotate_simple_left(root->left);
+    if(getFactor(root) > 0){
+        if(getFactor(root->right) == 1 || getFactor(root->right) == 0){
+            root = rotate_simple_left(root);
+        } else if(getFactor(root->right) == -1){
+            root = rotate_double_left(root);
         }
-        root = rotate_simple_right(root);
-    } else if(getFactor(root) == 2){
-        if(getFactor(root->right) == -1){
-            root->right = rotate_simple_right(root->right);
+    } else{
+        if(getFactor(root->left) == -1 || getFactor(root->left) == 0){
+            root = rotate_simple_right(root);
+        } else if(getFactor(root->left) == 1){
+            root = rotate_double_right(root);
         }
-        root = rotate_simple_left(root);
     }
 
     return root;
@@ -45,6 +75,14 @@ tree_avl rotate_simple_right(tree_avl root){
     root->left = temp->right;
     temp->right = root;
 
+    if(temp->factor == -1){
+        temp->factor = 0;
+        root->factor = 0;
+    } else {
+        temp->factor = 1;
+        root->factor = -1;
+    }
+
     return temp;
 }
 tree_avl rotate_simple_left(tree_avl root){
@@ -53,39 +91,121 @@ tree_avl rotate_simple_left(tree_avl root){
     root->right = temp->left;
     temp->left = root;
 
+    if(temp->factor == 1){
+        temp->factor = 0;
+        root->factor = 0;
+    } else {
+        temp->factor = -1;
+        root->factor = 1;
+    }
+
     return temp;
 }
 
-int getHeight(tree_avl root){
-    if(root == NULL)
-        return 0;
-    
-    return (maxHeight(getHeight(root->left), getHeight(root->right))+1);
+tree_avl rotate_double_left(tree_avl root) {
+    tree_avl temp_u, temp_v;
+
+    temp_u = root->right;
+    temp_v = temp_u->left;
+
+    temp_u->left = temp_v->right;
+    temp_v->right = temp_u;
+    root->right = temp_v->left;
+    temp_v->left = root;
+
+    if (temp_v->factor == -1) {
+        root->factor = 0;
+        temp_u->factor = 1;
+    } else if (temp_v->factor == 1) {
+        root->factor = -1;
+        temp_u->factor = 0;
+    } else {
+        root->factor = 0;
+        temp_u->factor = 0;
+    }
+    temp_v->factor = 0;
+
+    return temp_v;
 }
 
-int maxHeight(int a, int b){
-    return (a > b) ? a : b;
+tree_avl rotate_double_right(tree_avl root) {
+    tree_avl temp_u, temp_v;
+
+    temp_u = root->left;
+    temp_v = temp_u->right;
+
+    temp_u->right = temp_v->left;
+    temp_v->left = temp_u;
+    root->left = temp_v->right;
+    temp_v->right = root;
+
+    if (temp_v->factor == -1) {
+        root->factor = 0;
+        temp_u->factor = 1;
+    } else if (temp_v->factor == 1) {
+        root->factor = -1;
+        temp_u->factor = 0;
+    } else {
+        root->factor = 0;
+        temp_u->factor = 0;
+    }
+    temp_v->factor = 0;
+
+    return temp_v;
 }
 
 int getFactor(tree_avl root){
     if(root == NULL)
         return 0;
 
-    return(getHeight(root->right) - getHeight(root->left));
+    return root->factor;
 }
 
-tree_avl remove_avl(tree_avl root, int value){
+tree_avl remove_avl(tree_avl root, int value, int *remove_avl_factor){
     if(root == NULL){
         return root;
     }
 
     if(value < root->value){
-        root->left = remove_avl(root->left,value);
+        root->left = remove_avl(root->left,value, remove_avl_factor);
+        if(*remove_avl_factor == 1){
+            if(getFactor(root) == 1){
+                root->factor = 2;
+                if(getFactor(root->right) == 0){
+                    *remove_avl_factor = 0;
+                } else{
+                    *remove_avl_factor = 1;
+                }
+                root = balance_avl(root);
+            } else if(getFactor(root) == 0){
+                root->factor = 1;
+                *remove_avl_factor = 0;
+            } else{
+                root->factor = 0;
+                *remove_avl_factor = 1;
+            }
+        }
     }
     if(value > root->value){
-        root->right = remove_avl(root->right, value);
+        root->right = remove_avl(root->right, value, remove_avl_factor);
+        if(*remove_avl_factor == 1){
+            if(getFactor(root) == -1){
+                root->factor = -2;
+                if(getFactor(root->left) == 0){
+                    *remove_avl_factor = 0;
+                } else{
+                    *remove_avl_factor = 1;
+                }
+                root = balance_avl(root);
+            } else if(getFactor(root) == 0){
+                root->factor = -1;
+                *remove_avl_factor = 0;
+            } else{
+                root->factor = 0;
+                *remove_avl_factor = 1;
+            }
+        }
     }
-
     if(value == root->value){
         if((root->left == NULL && root->right == NULL) || root->left == NULL || root->right == NULL){
             if(root->right != NULL){
@@ -99,16 +219,34 @@ tree_avl remove_avl(tree_avl root, int value){
             } else{
                 root = NULL;
             }
+        *remove_avl_factor = 1;
         }
         else{
             tree_avl temp = root->left;
             temp = biggest_smallest_avl(temp);
             root->value = temp->value;
             temp->value = value;
-            root->left = remove_avl(root->left, value);
+            root->left = remove_avl(root->left, value, remove_avl_factor);
+            if(*remove_avl_factor){
+                if(getFactor(root) == -1){
+                    root->factor = 0;
+                    *remove_avl_factor = 0;
+                } else if(getFactor(root) == 0){
+                    root->factor = 1;
+                    *remove_avl_factor = 0;
+                } else{
+                    root->factor = 2;
+                    if(getFactor(root->right) == 0){
+                        *remove_avl_factor = 0;
+                    }else{
+                        *remove_avl_factor = 1;
+                    }
+                    root = balance_avl(root);
+                }
+            }
         }
     }
-    return balance_avl(root);
+    return root;
 }
 
 tree_avl biggest_smallest_avl(tree_avl root){
--- avl.h
+++ avl.h
@@ -9,11 +9,13 @@ typedef struct node_avl{
 
 typedef node_avl* tree_avl;
 
-tree_avl insert_avl(tree_avl root, int value);
-tree_avl remove_avl(tree_avl root, int value);
+tree_avl insert_avl(tree_avl root, int value, int *insert_avl_factor);
+tree_avl remove_avl(tree_avl root, int value, int *remove_avl_factor);
 tree_avl balance_avl(tree_avl root);
 tree_avl rotate_simple_right(tree_avl root);
 tree_avl rotate_simple_left(tree_avl root);
+tree_avl rotate_double_left(tree_avl root);
+tree_avl rotate_double_right(tree_avl root);
 int maxHeight(int a, int b);
 tree_avl biggest_smallest_avl(tree_avl root);
 int getHeight(tree_avl node);
